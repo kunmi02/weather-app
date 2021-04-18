e8bae0896b2e80115d0084b17b132317a53daae8
kunmi02 <mrkhunmie@gmail.com>
2021-04-18 17:40:09 +0100

    fix linter errors

--- src/modules/requester.js
+++ src/modules/requester.js
@@ -1,12 +1,9 @@
-const base_url = 'http://api.openweathermap.org/data/2.5/weather?q='
-const api_key = '63f5550851a7e639b98f0c7c3e0e2e06'
+const baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+const apiKey = '63f5550851a7e639b98f0c7c3e0e2e06';
 async function getWeatherData(city) {
-  // const response = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=YOUR_KEY_HERE&s=cats', {mode: 'cors'});
-  const response = await fetch(base_url+city+'&appid='+api_key);
+  const response = await fetch(`${baseUrl + city}&appid=${apiKey}`);
   const data = await response.json();
-  // console.log(data.coord);
-  return await data
-  
+  return data;
 }
 
 export default getWeatherData;
