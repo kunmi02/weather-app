fc97b397a855774858539ba06380fad150513677
kunmi02 <mrkhunmie@gmail.com>
2021-07-05 17:35:00 +0100

    changed http protocol

--- src/modules/requester.js
+++ src/modules/requester.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://api.openweathermap.org/data/2.5/find?q=';
+const baseUrl = 'https://api.openweathermap.org/data/2.5/find?q=';
 const apiKey = '63f5550851a7e639b98f0c7c3e0e2e06';
 async function getWeatherData(city, units) {
   try {
